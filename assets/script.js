5eae508d46d1c6625df32d46f0d3bea70669bc20
direwolf03633 <89884174+direwolf03633@users.noreply.github.com>
2021-09-30 21:24:59 -0700

    enter

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,46 @@
 // HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+console.log('Clever Cookie Bakery')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Luis Duran Garcia" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 0      // Ginger bread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+let gb = 0
+ onclick=submit
+function submit(id){ 
+var value = 1
+}    // Ginger bread
+let cc = 0 
+ onclick=submit
+function submit(id){
+var value = 2
+}     // Chocolate Chip
+let sugar = 0
+ onclick=submit
+function submit(id){
+var value = 1
+}   // Sugar Sprinkle
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
+document.getElementById('add-gb').addEventListener('click', function(){
+}   
+document.getElementById('add cc').addEventListener('click', function(){
+
+document.getElementById('add sugar').addEventListener('click', function(){
     // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+ var console: console.log('Ginger bread + button clicked!')
+ console.log ('Chocoloate chip + button clicked')
+ console.log('Sugar Sprinkle + button clicked')
 
     // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
+    let button = document.getElementById
+    button.context = 'Click me'
 
-// TODO: Hook up event listeners for the rest of the buttons
+console.log('Ginger bread + button clicked')
+// TODO: Hook up event listeners for the rest of the buttons\
+any.addEventListener('button', (local function)(); any
+button.context = 'Click me'
+}
